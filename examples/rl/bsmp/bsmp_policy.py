Fixed issue with scaling in bathces

examples/rl/bsmp/bsmp_policy.py
@@ -137,7 +137,7 @@ class BSMPPolicy(Policy):
     def compute_trajectory_from_theta(self, theta, context):
         q_0, q_d, q_dot_0, q_dot_d, q_ddot_0, q_ddot_d, puck = self.unpack_context(context)
         trainable_q_cps, trainable_t_cps = self.extract_qt(theta)
-        trainable_t_cps = trainable_t_cps + torch.log(1.33 * torch.ones_like(trainable_t_cps))
+        trainable_t_cps = trainable_t_cps #+ torch.log(1.33 * torch.ones_like(trainable_t_cps))
         #trainable_q_cps = torch.tanh(trainable_q_cps/10.) * np.pi
         middle_trainable_q_pts = torch.tanh(trainable_q_cps[:, :-3]/10.) * np.pi
         trainable_q_d = torch.tanh(trainable_q_cps[:, -1:]/10.) * np.pi
@@ -201,11 +201,29 @@ class BSMPPolicy(Policy):
         #q_cps = torch.cat(q_begin[:self._n_pts_fixed_begin] + [q_b + trainable_q_cps] + q_end[::-1], axis=-2)
 
         #q_cps = torch.cat(q_begin[:self._n_pts_fixed_begin] + [q_0 + torch.pi * trainable_q_cps] + q_end[:self._n_pts_fixed_end][::-1], axis=-2)
+        #q_cps = torch.tile(q_cps, (16, 1, 1))
+        #trainable_t_cps = torch.tile(trainable_t_cps, (16, 1, 1))
+        #trainable_t_cps += torch.randn_like(trainable_t_cps) * 0.15
         q, q_dot, q_ddot, t, dt, duration = self.compute_trajectory(q_cps.to(torch.float32), trainable_t_cps.to(torch.float32), differentiable=True)
-        q_dot_scale = (torch.abs(q_dot) / torch.tensor(self.joint_vel_limit)).max()
-        q_ddot_scale = (torch.abs(q_ddot) / torch.tensor(self.joint_acc_limit)).max()
-        scale = torch.maximum(q_dot_scale, q_ddot_scale**(1./2))
-        trainable_t_cps -= torch.log(scale)
+        #vscale = (torch.abs(q_dot) / torch.tensor(self.joint_vel_limit))
+        ##vscale_max = vscale.max()
+        #vscale_max = vscale.max(-1)[0].max(-1)[0]
+        #trainable_t_cps -= torch.log(vscale.max())
+        #q_, q_dot_, q_ddot_, t_, dt_, duration_ = self.compute_trajectory(q_cps.to(torch.float32), trainable_t_cps.to(torch.float32), differentiable=True)
+        #vscale_ = (torch.abs(q_dot_) / torch.tensor(self.joint_vel_limit))
+        #vscale_max_ = vscale_.max(-1)[0].max(-1)[0]
+        #colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+        #for i in range(7):
+        #    plt.plot(vscale[0, :, i].detach().numpy(), colors[i], label=f"joint {i}")
+        #    plt.plot(vscale_[0, :, i].detach().numpy(), colors[i] + '--', label=f"scaled joint {i}")
+        #plt.legend()
+        #plt.show()
+        q_dot_scale = (torch.abs(q_dot) / torch.tensor(self.joint_vel_limit))
+        q_ddot_scale = (torch.abs(q_ddot) / torch.tensor(self.joint_acc_limit))
+        q_dot_scale_max = torch.amax(q_dot_scale, (-2, -1), keepdim=True)
+        q_ddot_scale_max = torch.amax(q_ddot_scale, (-2, -1), keepdim=True)
+        scale_max = torch.maximum(q_dot_scale_max, q_ddot_scale_max**(1./2))
+        trainable_t_cps -= torch.log(scale_max)
         q, q_dot, q_ddot, t, dt, duration = self.compute_trajectory(q_cps.to(torch.float32), trainable_t_cps.to(torch.float32), differentiable=True)
         self._traj_no += 1
         return q, q_dot, q_ddot, t, dt, duration